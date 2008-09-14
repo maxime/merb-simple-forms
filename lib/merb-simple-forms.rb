e145855764f8d8e148a86aff353d369401359ee9
Maxime <maxime@maxime.local>
2008-09-14 22:05:23 +0200

    Remove loading log output

--- lib/merb-simple-forms.rb
+++ lib/merb-simple-forms.rb
@@ -1,6 +1,5 @@
 # make sure we're running inside Merb
 if defined?(Merb::Plugins)
-  Merb.logger.info "loading simple forms helpers"
   require Pathname(__FILE__).dirname.expand_path / 'merb-simple-forms' / 'simple-forms-helpers.rb'
 
   Merb::BootLoader.before_app_loads do
